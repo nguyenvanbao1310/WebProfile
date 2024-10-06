b82fbf8f010d747861ac8e0ce18b140f2fc5c881
nguyenvanbao <nguyenhoanggiaphong12@gmail.com>
2024-10-06 22:39:44 +0700

    update

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -48,14 +48,18 @@ const Exercise = () => {
       title: "EXERCISE 4 (Week6)   ",
       description: "Cart",
       image: tuan5,
-<<<<<<< HEAD
       linkgit:
         "https://github.com/nguyenvanbao1310/BaiTapLTWeb/tree/master/ch07",
       linkweb: "https://ch07-cart.onrender.com",
-=======
-      linkgit: "",
-      linkweb: "",
->>>>>>> 8c715441252c7e16e0161243ed18c50557665a79
+    },
+    {
+      id: 6,
+      title: "EXERCISE 5 (Week7)",
+      description: "How to use JDBC to work with a database",
+      image: tuan1,
+      linkgit:
+        "https://github.com/nguyenvanbao1310/BaiTapLTWeb/tree/master/ch12",
+      linkweb: "https://ch12-v27l.onrender.com",
     },
   ]);
   const handleNextClick = () => {
